ede9398d7a1b3c0273c27f664cb7ed2bbba994d9
gymcode <kyleabs20@gmail.com>
2022-05-07 23:21:55 +0000

    [endpoint for random data | endpoint for search]

---
+++ src/services/endpoints.js
@@ -0,0 +1,6 @@
+const Endpoints = {
+    SEARCH: "https://api.spoonacular.com/recipes/complexSearch",
+    RANDOM_RECIPES: "https://api.spoonacular.com/recipes/random?number=3"
+}
+
+export default Endpoints
