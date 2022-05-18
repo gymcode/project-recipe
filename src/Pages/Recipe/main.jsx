5dbb5ba1a1ffb825ee214c07200741241a44ff9d
gymcode <kyleabs20@gmail.com>
2022-05-18 21:54:52 +0000

    sorted the information image out

--- src/Pages/Recipe/main.jsx
+++ src/Pages/Recipe/main.jsx
@@ -8,9 +8,27 @@ function MainComponent() {
   const {id} = useParams()
   const dataObject = useFetch(Endpoints.RECIPE_INFO(id), {isLoading: false, data: null}, "recipe_information")
   console.log(dataObject)
+  const recipeImage = dataObject.data.image
+  const bgImage = {
+    backgroundImage: `url(${recipeImage})`,
+    clipPath: "circle(67% at 28% 16%)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  }
+  
   return (
-    <div className="recipe-container grid grid-rows-7">
-      
+    <div className="recipe-container grid grid-cols-2">
+      <div className="grid grid-rows-3">
+        <div className="row-span-2" style={bgImage}>
+
+        </div>
+        <div>das</div>
+      </div>
+      <div className="grid grid-rows-2">
+        <div>dsdas</div>
+        <div>das</div>
+      </div>
     </div>
   );
 }
