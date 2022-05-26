af3077c94bd9eb4821d284e944e3deb5c3c79473
gymcode <kyleabs20@gmail.com>
2022-05-26 00:28:21 +0000

    first part of responsiveness done

--- src/Components/card.js
+++ src/Components/card.js
@@ -10,30 +10,30 @@ function Card({ id, title, image, score, Vegetarian, gluten, time }) {
       className="bg-white card-style shadow-xl lg:mt-20 xs:mt-7 rounded-lg grid lg:grid-cols-2 
                     lg:grid-rows-none sm:grid-rows-2 xs:grid-cols-2 border-black lg:h-3/4 lg:w-1/10 xs:w-full xs:h-48"
     >
-      <div className="lg:h-full lg:w-full xs:h-3/4">
+      <div className="lg:h-full lg:w-full">
         <img
           src={image}
-          className={"lg:h-full lg:w-full xs:h-3/4 rounded-lg"}
+          className={"lg:h-full lg:w-full xs:h-48 rounded-lg"}
           style={{
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         />
       </div>
-      <div className="h-full w-full p-5 grid grid-row-5">
+      <div className="h-full w-full lg:p-5 xs:p-2 grid grid-row-5">
         <div className="flex justify-center items-center row-span-3">
-          <h2 className="text-2xl capitalize text-center text-white h-16">
+          <h2 className="lg:text-2xl xs:text-md capitalize text-center text-white lg:h-16 xs:h-5">
             {title}
           </h2>
         </div>
         <div
-          className="row-span-1 grid grid-rows-2"
+          className="row-span-1 grid lg:grid-rows-2"
           style={{ borderTop: "1px solid rgba(214, 214, 214, 0.11)" }}
         >
-          <div className="flex justify-around items-center">
+          <div className="lg:flex lg:justify-around lg:items-center hidden">
             <div>
               <div className="flex justify-center">
-                <div className="h-3 w-3 bg-green-600 rounded mr-2 mt-1" />
+                <div className="bg-green-600 rounded mr-2 mt-1" style={{height: "0.75rem", width: "0.75rem"}} />
                 <div>
                   <h2 className="text-white">Health score</h2>
                   <p className="text-sm text-gray-400">{score}%</p>
@@ -42,7 +42,7 @@ function Card({ id, title, image, score, Vegetarian, gluten, time }) {
             </div>
             <div>
               <div className="flex justify-center">
-                <div className="h-3 w-3 bg-blue-600 rounded mr-2 mt-1" />
+                <div className="bg-blue-600 rounded mr-2 mt-1" style={{height: "0.75rem", width: "0.75rem"}}  />
                 <div>
                   <h2 className="text-white">Cooking time</h2>
                   <p className="text-sm text-gray-400">{time}mins</p>
@@ -50,7 +50,7 @@ function Card({ id, title, image, score, Vegetarian, gluten, time }) {
               </div>
             </div>
           </div>
-          <div className="flex justify-around items-center">
+          <div className="flex lg:flex-row xs:flex-col justify-around items-center">
             <div className="flex text-white ml-4">
               Vegetarian
               <div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
+    extend: {
+      width: {
+        '1/10' : "93%"
+      }
+    },
     screens: {
       'xs': '320px',
       ...defaultTheme.screens
     },
-    width: {
-      '1/10' : "93%"
-    }
   },
   plugins: [require("flowbite/plugin")],
 };
