f5f74af8bfad17ab8a9ca767ba37064c96a30fea
gymcode <kyleabs20@gmail.com>
2022-05-26 23:21:34 +0000

    more responsiveness

--- src/Pages/Home/main.jsx
+++ src/Pages/Home/main.jsx
@@ -47,7 +47,7 @@ function MainComponent() {
   return (
     <div
       className={
-        "main-container grid grid-rows-2 grid-flow-col lg:p-20 md:p-20 sm:p-16 xs:p-12"
+        "main-container grid grid-rows-2 grid-flow-col xl:p-20 lg:16 md:p-16 sm:p-16 xs:p-12"
       }
     >
       {/* responsiveness  */}
@@ -56,7 +56,7 @@ function MainComponent() {
           <div>
             <h3
               className={
-                "kreon-font capitalize lg:text-8xl md:text-8xl sm:text-4xl xs:text-4xl text-white"
+                "kreon-font capitalize xl:text-8xl lg:text-7xl md:text-6xl sm:text-4xl xs:text-4xl text-white"
               }
             >
               Simple and <br />
@@ -68,13 +68,13 @@ function MainComponent() {
             <img
               src={logo}
               className={
-                "lg:h-24 lg:w-24 md:h-32 md:w-32 sm:h-28 sm:w-28 xs:h-12 w-12"
+                "xl:h-24 xl:w-24 md:h-32 md:w-32 sm:h-28 sm:w-28 xs:h-12 w-12"
               }
               alt=""
             />
           </div>
         </div>
-        <div className={"py-12 lg:w-[40%]  px-5"}>
+        <div className={"py-12 xl:w-[40%] lg:w-[50%] px-5"}>
           <p className={"dancing-script text-xl text-white pb-3"}>
             <q>{quote.content}</q>
           </p>
@@ -82,12 +82,12 @@ function MainComponent() {
         </div>
       </div>
       <div className={"kreon-font"}>
-        <h4 className="capitalize text-xl lg:mt-1 sm:text-md xs:text-md text-white xs:-mt-20">Top Trends ....</h4>
+        <h4 className="capitalize text-xl xl:mt-1 sm:text-md xs:text-md text-white xs:-mt-20">Top Trends ....</h4>
         <div
-          className={"grid lg:grid-cols-6 xs:grid-cols-1 sm:grid-cols-1 gap-8 lg:px-5 xs:px-1 h-full flex justify-center"}
+          className={"grid xl:grid-cols-6 xs:grid-cols-1 sm:grid-cols-1 gap-8 xl:px-5 xs:px-1 h-full flex justify-center"}
         >
           {/* random recipes iterations  */}
-          <div className={"col-span-5 grid lg:grid-cols-2 lg:grid-rows-none xs:grid-cols-none xs:grid-rows-2"}>
+          <div className={"col-span-5 grid xl:grid-cols-2 xl:grid-rows-none md:grid-cols-2 md:grid-rows-none sm:grid-cols-none sm:grid-rows-2 xs:grid-cols-none xs:grid-rows-2"}>
             {dataResponse.isLoading ? (
               <div>
                 <Lottie options={defaultLoadingOptions} />
@@ -110,7 +110,7 @@ function MainComponent() {
           </div>
           {/* arrow navigation for next page */}
           <Link to="/healthy">
-            <div className={"lottie lg:block md:block xs:hidden"}>
+            <div className={"lottie xl:block lg:hidden md:hidden xs:hidden"}>
               <Lottie options={defaultOptions} />
             </div>
           </Link>
