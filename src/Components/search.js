1685b6a10ec44f2d46c8b05223161d7e26794c52
gymcode <kyleabs20@gmail.com>
2022-05-29 23:17:03 +0000

    search bar complete

--- src/Components/search.js
+++ src/Components/search.js
@@ -1,9 +1,20 @@
-const Search = ()=>{
-    return (
-        <div className="bg-red-300 w-auto h-16 flex items-center w-[100%]">
-            ddad
-        </div>
-    )
-}
+import { ReactComponent as SearchVector } from "../Assets/Images/Vector.svg";
 
-export default Search
+const Search = () => {
+  return (
+    <div className="w-auto h-20 pl-10 flex items-center w-[100%]">
+      <div className="flex items-center bg-gray-100 w-11/12 py-1 rounded-md divide-x">
+        <input
+          className="ml-3 h-8 outline-0 bg-transparent text-gray-500"
+          style={{ width: "95%" }}
+          placeholder={"search..."}
+        />
+        <div className="border-l border-gray-400 px-3">
+            <SearchVector height={"1.3rem"}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
