c0308575f0f9a4d7e5a3e786c3d7b29d4a57a10c
gymcode <kyleabs20@gmail.com>
2022-05-29 23:38:11 +0000

    done with first section

--- src/Components/search.js
+++ src/Components/search.js
@@ -2,7 +2,7 @@ import { ReactComponent as SearchVector } from "../Assets/Images/Vector.svg";
 
 const Search = () => {
   return (
-    <div className="w-auto h-20 pl-10 flex items-center w-[100%]">
+    <div className="w-auto h-20 pl-10 flex items-center w-[100%] border-b border-gray-100">
       <div className="flex items-center bg-gray-100 w-11/12 py-1 rounded-md divide-x">
         <input
           className="ml-3 h-8 outline-0 bg-transparent text-gray-500"
@@ -10,7 +10,7 @@ const Search = () => {
           placeholder={"search..."}
         />
         <div className="border-l border-gray-400 px-3">
-            <SearchVector height={"1.3rem"}/>
+            <SearchVector height={"1.1rem"}/>
         </div>
       </div>
     </div>
