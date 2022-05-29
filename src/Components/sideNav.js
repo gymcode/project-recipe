ebd1447055e88245c69a71e5d026ddbee0b19ba3
gymcode <kyleabs20@gmail.com>
2022-05-29 18:33:08 +0000

    side navigation

---
+++ src/Components/sideNav.js
@@ -0,0 +1,9 @@
+const SideNav = ()=>{
+    return (
+        <div className="w-1/6 side-nav-container bg-red-400 xl:block lg:bloxk md:block sm:hidden xs:hidden">
+            
+        </div>
+    )
+}
+
+export default SideNav
--- src/Pages/Category/main.jsx
+++ src/Pages/Category/main.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 import "./main.css";
 import Lottie from "react-lottie";
-import { SEARCH } from "../../services/endpoints";
-import { useFetch } from "../../Hooks";
+import SideNav from "../../Components/sideNav";
 
 function MainComponent() {
   return (
     <div className="category-container grid grid-rows-7">
-      
+      {/* side navigation  */}
+      <SideNav>
+        
+      </SideNav>
     </div>
   );
 }
