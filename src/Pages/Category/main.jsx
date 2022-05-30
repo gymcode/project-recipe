74735dad33a82b24d3a2051aa1fe776faac5c402
gymcode <kyleabs20@gmail.com>
2022-05-30 23:35:32 +0000

    moving on to the next section

--- src/Pages/Category/main.jsx
+++ src/Pages/Category/main.jsx
@@ -22,14 +22,26 @@ function MainComponent() {
           <div className="p-10 ml-1 mt-20">
             {/* render random components in this section */}
             <div className="">
-              <div className="post-header-img rounded-2xl w-2/3 bg-green-500 flex">
+              <div className="post-header-img rounded-2xl w-2/3 flex">
                 <div className="w-[62%] p-10 text-white">
-                  <h2 className="text-5xl kreon-font py-3">Hey there</h2>
-                  <p className="leading-none cookie-font text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, incidunt! Sit rerum harum repellat qui voluptas accusamus non itaque veniam maiores, laborum atque. Dolore eum harum natus velit, saepe quaerat.</p>
+                  <h2 className="text-5xl kreon-font py-3">Hey there,</h2>
+                  <p className="leading-none cookie-font text-lg text-gray-900">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Hic, incidunt! Sit rerum harum repellat qui voluptas
+                    accusamus non itaque veniam maiores, laborum atque. Dolore
+                    eum harum natus velit, saepe quaerat.
+                  </p>
                 </div>
                 <div className="w-[38%] ">
-                  <img src={CookingFrame} className={"absolute w-[21%] top-3"} alt="" />
+                  <img
+                    src={CookingFrame}
+                    className={"absolute w-[21%] top-3"}
+                    alt=""
+                  />
                 </div>
+              </div>
+              <div className="my-5">
+                <h2 className="kreon-font">Let's guess some dishes you'll like to prepare...</h2>
               </div>
             </div>
           </div>
