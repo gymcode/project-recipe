d124e1df24009f35bb15337e6a85676cbe5252b1
gymcode <kyleabs20@gmail.com>
2022-05-30 22:22:26 +0000

    corrected postioning issue

--- src/Pages/Category/main.jsx
+++ src/Pages/Category/main.jsx
@@ -6,20 +6,20 @@ import Search from "../../Components/search";
 
 function MainComponent() {
   return (
-    <div className="category-container">
+    <div className="category-container overscroll-none">
       <div className="grid grid-cols-6 h-full">
         {/* side navigation section  */}
         <div className="">
           <SideNav />
         </div>
         {/* main body section  */}
-        <div className="col-span-5">
+        <div className="col-span-5 relative">
           {/* search section  */}
-          <div>
+          <div className="absolute top-0 left-0 right-0">
             <Search />
           </div>
           {/* dynamic section  */}
-          <div className="p-10 ml-1">
+          <div className="p-10 ml-1 mt-20">
             csdas
           </div>
         </div>
